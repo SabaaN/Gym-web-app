f1e4fb4b945454a8f3b4db0ace283358fbc8b8bb
Saba N <91379786+SabaaN@users.noreply.github.com>
2024-01-20 22:22:11 +0500

    Update ExerciseDetail.js

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -6,7 +6,7 @@ import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
-import SimilarExercises from '../components/SimilarExercisess';
+import SimilarExercises from '../components/SimilarExercises';
 
 
 const ExerciseDetail = () => {
